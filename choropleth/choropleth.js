9fa816f320f2d9f3b875668295fbccaa0f15a01d
Your Name <you@example.com>
2022-04-13 01:40:32 -0700

    created tooltip

--- choropleth/choropleth.js
+++ choropleth/choropleth.js
@@ -19,7 +19,7 @@ function main() {
         .rangeRound([600, 860])
 
     var color = d3.scaleThreshold()
-        .domain([10,20,30,40,50,60,70,80,90,100])
+        .domain([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
         .range(d3.schemeBlues[3])
 
     container_g.selectAll("rect")
@@ -27,34 +27,54 @@ function main() {
             d = color.invertExtent(d);
             if (d[0] == null) d[0] = x.domain()[0];
             if (d[1] == null) d[1] = x.domain()[1];
-            console.log(d[1])
             return d;
         }))
 
-    container_g.append("text")
-        .attr('class', "caption")
-        .attr("x", x.range()[0])
-        .attr("y", -6)
-        .attr("fill", "#000")
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text("Poverty Rate")
-
-    container_g.call(d3.axisBottom(x)
-        .tickSize(10)
-        .tickFormat(function (x, i) { return i ? x : x + "%" })
-        .tickValues(color.domain()))
-        .select(".domain")
-        .remove();
-
     var promises = [
-        d3.json("https://d3js.org/us-10m.v1.json"),
-        d3.csv("../CSV/PovertyData.csv", function (d) { poverty.set(d.FIPStxt, +d.Percentage);})
+        d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json"),
+        d3.csv("../CSV/PovertyData.csv", function (d) { poverty.set(d.FIPStxt, +d.Percentage,d.State); })
     ]
 
     Promise.all(promises).then(ready)
 
     function ready([us]) {
+
+        var toolTip = d3.select("#choropleth_map")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("position", "absolute")
+            .style("font-family", "sans-serif")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+
+        var mouseover = function (d) {
+            toolTip
+                .style("opacity", 1)
+            d3.select(this)
+                .raise()
+                .classed("active", true)
+            d3.select(this)
+                .style("opacity", 0.75)
+        }
+
+        var mousemove = function (d) {
+            toolTip
+                .html("County: " + d.properties.name + "<br>Cost Index: " + poverty.get(d.id))
+                .style("left", (d3.mouse(this)[0] + 70) + "px")
+                .style("top", (d3.mouse(this)[1]) + "px")
+
+        }
+
+        var mouseleave = function (d) {
+            toolTip
+                .style("opacity", 0)
+            d3.select(this)
+                .style("opacity", 1)
+        }
         svg.append("g")
             .attr("class", "counties")
             .selectAll("path")
@@ -62,6 +82,10 @@ function main() {
             .enter().append("path")
             .attr("fill", function (d) { return color(poverty.get(d.id)); })
             .attr("d", path)
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+
 
         svg.append("path")
             .datum(topojson.mesh(us, us.objects.states, function (a, b) {
